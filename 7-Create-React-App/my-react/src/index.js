6caec85c2bc67efbb161f80dc1785a73e65ca3e0
Anil Gunjal <anilgunjal450@gmail.com>
2020-11-25 20:11:39 +0530

    Styling in React

--- 7-Create-React-App/my-react/src/index.js
+++ 7-Create-React-App/my-react/src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Appclass from './Appclass';
+import App from './App';
+//import Appclass from './Appclass';
 import reportWebVitals from './reportWebVitals';
-
 ReactDOM.render(
   <React.StrictMode>
-    <Appclass />
+    <App name= "Anil Gunjal" />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
