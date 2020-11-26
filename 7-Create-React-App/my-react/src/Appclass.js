8281a873a8a135f65eb7f8dda24b24d1254a6274
Anil Gunjal <anilgunjal450@gmail.com>
2020-11-26 20:25:53 +0530

    Bind and handleClick Function

--- 7-Create-React-App/my-react/src/Appclass.js
+++ 7-Create-React-App/my-react/src/Appclass.js
@@ -5,15 +5,26 @@ class Appclass extends React.Component {
   constructor(props){
     super(props)
     this.state = {isShow:true};
-
+    this.handleClick = this.handleClick.bind(this);
     // states are immutable means you can't change the state directly
+  }
+
+  // handleClick = () =>{
+  //   this.setState({isShow:!this.state.isShow});
+  // }
+    
+  // if we use the Norman function handliClick(){} then it will give the error like can not read property setState of undefined because function won't getting isShow.
+  // To solve this we are using bind function, for Arrow function not needed the bind function.
+
+  handleClick(){
+    this.setState({isShow:!this.state.isShow});
   }
   render() {
     return (
       <div className="mainDiv">
         <div className="textDiv">
           <h3>Onclick Event</h3>
-          <button onClick={ () =>  this.setState({isShow:!this.state.isShow})}>Toggle</button>
+          <button onClick={this.handleClick}>Toggle</button>
         </div>
         { this.state.isShow ? (<img src="https://images.unsplash.com/photo-1604423203943-54721eff418a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1049&q=80" />) : null}
         
